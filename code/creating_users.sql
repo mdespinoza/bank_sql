8cb5a3055ddf84b8e4f9c88e3075872d4019bed5
mdespinoza <markdespinoza@outlook.com>
2023-03-24 00:56:59 -0500

    creaitng user code updated

--- code/creating_users.sql
+++ code/creating_users.sql
@@ -6,12 +6,11 @@ GRANT ALL PRIVILEGES ON * . * TO 'user'@'localhost';
 FLUSH PRIVILEGES;
 
 -- @block: creating another user that only has access to the database 'bank_db' with all privileges and can only be accessed from the localhost
-CREATE USER 'bank_user'@'localhost' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON bank_db.* TO 'bank_user'@'localhost';
+CREATE USER 'user 1'@'localhost' IDENTIFIED BY 'Password123!';
+GRANT ALL PRIVILEGES ON bank_db.* TO 'user 1'@'localhost';
 FLUSH PRIVILEGES;
 
 -- @block: creating another user that only has access to the database 'bank_db' with all privileges and can be accessed from any host
--- create a user that can access with any host
 CREATE USER 'bank_user'@'%' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON bank_db.* TO 'bank_user'@'%';
 FLUSH PRIVILEGES;
